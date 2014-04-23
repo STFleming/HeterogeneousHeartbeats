c486d0be0a01aed01fd0ca46b3d2d6f6978c2f76
Shane Fleming <shane.fleming06@imperial.ac.uk>
2014-04-23 16:00:13 +0000

    Added header file used for creating the application list.

---
+++ software/linux/userspace/includes/hhb_applist.c
@@ -0,0 +1,128 @@
+#include "hhb_applist.h"
+
+//This function checks to see if a shared memory exists for the application lists.
+//If one does exist then it creates the shared memory.
+//This is used as helper function 
+applist_state_t* applist_fetch_list_state(void)
+{
+	applist_state_t *p = NULL;
+
+	int shmid;
+	struct shmid_ds buf;
+
+	if ((shmid = shmget(APPLIST_SHM_STATE_KEY, sizeof(applist_state_t), IPC_CREAT | 0666)) < 0) {
+		perror("cannot allocate shared memory for the applications list");
+		p = NULL;
+	}
+
+	  if ((p = (applist_state_t*) shmat(shmid, NULL, 0)) == (applist_state_t *) -1) {
+    		perror("cannot attach shared memory for the application list to the heartbeat enabled process");
+		p = NULL;
+	}
+
+	if(shmctl(shmid, SHM_LOCK, &buf) < 0)
+	{
+		p = NULL;
+	}
+
+	p->state_shmid = shmid;
+	p->list_head = applist_fetch_list();	
+
+	return p;
+};
+
+//Helper function used to create or get the list of applications
+applist_entry_t* applist_fetch_list(void)
+{
+	applist_entry_t *p = NULL;
+
+        int shmid;
+        struct shmid_ds buf;
+        
+        if ((shmid = shmget(APPLIST_SHM_KEY, LIST_SIZE*sizeof(applist_entry_t), IPC_CREAT | 0666)) < 0) {
+                perror("cannot allocate shared memory for the applications list");
+                p = NULL;
+        }
+
+          if ((p = (applist_entry_t*) shmat(shmid, NULL, 0)) == (applist_entry_t *) -1) {
+                perror("cannot attach shared memory for the application list to the heartbeat enabled process");
+                p = NULL;
+        }
+
+        if(shmctl(shmid, SHM_LOCK, &buf) < 0)
+        {
+                p = NULL;
+        }
+
+	return p;
+}
+
+//This function is used to create a SW applist entry that can be appended to the shared applist
+applist_entry_t* applist_create_sw_entry(int64_t in_app_state_phys_addr, int64_t in_app_log_phys_addr)
+{
+	applist_entry_t * p;
+	p->AppID = getpid();
+	p->HW_SW = 0; //The application is resident in SW
+	p->app_state_phys_addr = in_app_state_phys_addr;
+	p->app_log_phys_addr = in_app_log_phys_addr;
+	p->alive = 1; //Yes the app is currently running
+	
+	return p;
+}
+
+
+//This application searches through the application list and adds the app to the list
+//when it finds the first free location
+void applist_register_app(applist_entry_t * new_app)
+{
+	applist_state_t* app_state = applist_fetch_list_state(); //Get the current application state
+	int i = 0; //This is a counter that is used to iterate through the list
+	int success = 0;	
+
+	for(i=0; i<LIST_SIZE; i++)
+	{
+		if( app_state->list_head[i].alive == 0) //This means that we have located a free spot
+		{
+			app_state->list_head[i].AppID = new_app->AppID; //Append the data to the application list
+			app_state->list_head[i].HW_SW = new_app->HW_SW;
+			app_state->list_head[i].app_state_phys_addr = new_app->app_state_phys_addr;
+			app_state->list_head[i].app_log_phys_addr = new_app->app_log_phys_addr;
+			app_state->list_head[i].alive = new_app->alive;
+			success = 1;		
+			break; //We can stop searching now and do not want to add the application multiple times.
+		}
+	} 
+
+	if(success != 0) 
+	{
+		printf("Error!! Not enough free space in the application list, try increasing the LIST_SIZE preprocessor directive.\n");		
+	}
+	
+	return;
+}
+
+//This function searches through the application list and removes the elements specified in the input argument.
+void applist_remove_app(int input_AppID)
+{
+	applist_state_t* app_state = applist_fetch_list_state();
+	int i=0;
+	int success = 0;
+	
+	for(i=0;i<LIST_SIZE; i++)
+	{
+		if(app_state->list_head[i].AppID == input_AppID)
+		{
+			//We have a match
+			app_state->list_head[i].alive = 0;
+			success = 1;
+			break;
+		}
+	}
+	
+	if(success != 1)
+	{
+		printf("Error!! The application could not be located in the list, and therfore could not be removed\n");
+	}
+	
+	return;
+}
---
+++ software/linux/userspace/includes/hhb_applist.h
@@ -0,0 +1,41 @@
+#include "heartbeat.h"
+
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <sys/shm.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#ifndef _APPLIST_H_
+#define _APPLIST_H_
+
+#define LIST_SIZE 20
+#define APPLIST_SHM_STATE_KEY 6528745
+#define APPLIST_SHM_KEY 6528756
+
+typedef struct {
+	int AppID; //The application ID, this is the PID number for SW application and is user specified for HW applications.
+	int HW_SW; //Set to 1 if the application is in HW, otherwise it is set to 0
+	int64_t app_state_phys_addr; //The physical address for the shared state memory
+	int64_t app_log_phys_addr; //The physical address for the shared log memory
+	int alive; //Set to 1 when the application actually exists in the system, 0 if not	
+} applist_entry_t;
+
+typedef struct {
+	int state_shmid; //The ID for the shared memory.
+	int list_shmid;
+	applist_entry_t * list_head; //A pointer to the head of the application list	
+	int lock_aquired; //This is to ensure mutual exclusive access
+} applist_state_t;
+
+applist_state_t* applist_fetch_list_state(void); //Helper function used to allocate the shared memory space
+
+applist_entry_t* applist_fetch_list(void); //Helper function used to get the list of applications
+
+applist_entry_t* applist_create_sw_entry(int64_t app_state_phys_addr, int64_t app_state_log_addr); 
+
+void applist_register_app(applist_entry_t * new_app); //Used to search through the application list for a free spot and add the calling application to it.
+
+void applist_remove_app(int input_AppID); //Used to remove an app from the application list.
+
+#endif
