7b152c9086c464fcf7adb26758aea60fb37b2aba
Felix Winterstein <f.winterstein12@imperial.ac.uk>
2014-06-19 15:49:59 +0100

    back to original version

--- applications/lloyds/kernel/HLS/source/lloyds_kernel_top.cpp
+++ applications/lloyds/kernel/HLS/source/lloyds_kernel_top.cpp
@@ -84,7 +84,7 @@ void store_output_points_buffer(uint offset, data_type *buffer, uint address, vo
 
 }
 
-/*
+
 // top-level function of the design
 void lloyds_kernel_top(  uint block_address,
 						 volatile bus_type *master_portA,
@@ -148,6 +148,15 @@ void lloyds_kernel_top(  uint block_address,
 	load_centres_buffer(centres_in_addr, 0, master_portA, k, centres_buffer);
 	*debug = data_points_buffer[0].value[0];
 
+	/*
+	for (uint i=0; i<B; i++) {
+		data_type u = data_points_buffer[i];
+		printf("%d %d %d\n", u.value[0], u.value[1], u.value[2]);
+	}
+	*/
+
+
+
     // iterate over all data points
     process_data_points_loop: for (uint i=0; i<B; i++) {
 
@@ -187,7 +196,7 @@ void lloyds_kernel_top(  uint block_address,
 		}
 
         output_buffer[i].min_idx = final_centre_index;
-        output_buffer[i].sum_sq = 0;//sum_sq_out;
+        output_buffer[i].sum_sq = sum_sq_out;
 
         output_points_buffer[i] = closest_centre;
 
@@ -203,79 +212,10 @@ void lloyds_kernel_top(  uint block_address,
     } else {
     	store_output_points_buffer(output_addr, output_points_buffer, data_points_block_address, master_portA);
     }
-}
-
-*/
-
-
-// top-level function of the design
-void lloyds_kernel_top(  uint block_address,
-						 volatile bus_type *master_portA,
-						 //volatile bus_type *master_portB,
-						 uint data_points_addr,
-                         uint centres_in_addr,
-                         uint output_addr,
-                         uint update_points,
-                         uint n,
-                         uint k, //changed so that the AXI slave interface can be used.
-						 uint *debug
-                         )
-{
-
-	// set up the axi bus interfaces
-	#pragma HLS INTERFACE ap_bus port=master_portA depth=0x200000
-	#pragma HLS resource core=AXI4M variable=master_portA
-
-	//#pragma HLS INTERFACE ap_bus port=master_portB depth=0x002000
-	//#pragma HLS resource core=AXI4M variable=master_portB
-
-	#pragma HLS INTERFACE ap_none register port=data_points_addr
-	#pragma HLS RESOURCE core=AXI4LiteS variable=data_points_addr metadata="-bus_bundle CONFIG_BUS"
-
-	#pragma HLS INTERFACE ap_none register port=centres_in_addr
-	#pragma HLS RESOURCE core=AXI4LiteS variable=centres_in_addr metadata="-bus_bundle CONFIG_BUS"
-
-	#pragma HLS INTERFACE ap_none register port=output_addr
-	#pragma HLS RESOURCE core=AXI4LiteS variable=output_addr metadata="-bus_bundle CONFIG_BUS"
-
-	#pragma HLS INTERFACE ap_none register port=n
-	#pragma HLS RESOURCE core=AXI4LiteS variable=n metadata="-bus_bundle CONFIG_BUS"
-
-	#pragma HLS INTERFACE ap_none register port=k
-	#pragma HLS RESOURCE core=AXI4LiteS variable=k metadata="-bus_bundle CONFIG_BUS"
-
-	#pragma HLS INTERFACE ap_none register port=update_points
-	#pragma HLS RESOURCE core=AXI4LiteS variable=update_points metadata="-bus_bundle CONFIG_BUS"
-
-	#pragma HLS INTERFACE ap_none register port=block_address
-	#pragma HLS RESOURCE core=AXI4LiteS variable=block_address metadata="-bus_bundle CONFIG_BUS"
-
-	#pragma HLS INTERFACE ap_none register port=debug
-	#pragma HLS RESOURCE core=AXI4LiteS variable=debug metadata="-bus_bundle CONFIG_BUS"
-
-	#pragma HLS RESOURCE variable=return core=AXI4LiteS	metadata="-bus_bundle CONFIG_BUS"
-
-	data_type data_points_buffer[B];
-	data_type output_points_buffer[B];
-
-
-	//void load_points_buffer(uint offset,uint address, volatile bus_type *bus, data_type *buffer)
-	//void load_centres_buffer(uint offset, uint address, volatile bus_type *bus, centre_index_type k, data_type *buffer)
-
-	for (uint ba=0; ba < n; ba+=B) {
-
-		uint data_points_block_address = D*ba;
-
-		load_points_buffer(data_points_addr, data_points_block_address, master_portA, data_points_buffer);
-		*debug = data_points_buffer[0].value[0];
-
-		store_output_points_buffer(output_addr, output_points_buffer, data_points_block_address, master_portA);
-
-	}
-
 
 
 
 
 }
+
 
